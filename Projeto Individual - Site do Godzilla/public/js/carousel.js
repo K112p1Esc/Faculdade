6d7391d34f7d6f5d3eae31949995d0428b5c163c
K112p1Esc <matheus.fsilva@sptech.school>
2026-04-24 14:02:51 -0300

    Transferencia de FOREACH para FOR do carousel e criação base da estrutura de filmes

--- Projeto Individual - Site do Godzilla/public/js/carousel.js
+++ Projeto Individual - Site do Godzilla/public/js/carousel.js
@@ -1,39 +1,31 @@
-// Seleciona TODAS as imagens do carousel (classe .slide)
-// Para então virar uma NODE LIST (coleção de objetos para usar o querySelectorALl) com o document (seleciona a tag)
 let slides = document.querySelectorAll(".slide");
-// Seleciona todos os indicadores (que ta como indica, a bolinha!)
-let dots = document.querySelectorAll(".indica");
-// É o índice da primeira imagem (que é 0 né)
-let index = 0;
+let bolinha = document.querySelectorAll(".indica");
+let i = 0;
 
-// Função que avança para o próximo slide
 function nextSlide() {
-    // Aumenta o meu indice
-    index++;
-    // Ve se passou da última imagem para então voltar a primeira
-    if (index >= slides.length) {
-        // O loop ai
-        index = 0;
+    i++;
+    if (i >= slides.length) {
+        i = 0;
     }
-    // Mostra o slide atual invocando a função
-    showSlide(index);
+    showSlide(i);
 }
-// Executa a função nextSlide a cada 4 segundos, que é 4000 ms em JS
-// Ai fica parecendo que é automatico
+
 setInterval(nextSlide, 4000);
 
-// Função que mostra o slide
 function showSlide(i) {
+    // Remove "active" de todos os slides
+    for (let j = 0; j < slides.length; j++) {
+        slides[j].classList.remove("active");
+    }
 
-    // Remove a classe "active" de TODAS as imagens
-    slides.forEach(slide => slide.classList.remove("active"));
+    // Remove "active" de todos os bolinha
+    for (let j = 0; j < bolinha.length; j++) {
+        bolinha[j].classList.remove("active");
+    }
 
-    // Faz o mesmo que nas imagens só que com as bolinhas
-    dots.forEach(indica => indica.classList.remove("active"));
-
-    // Adiciona "active" SOMENTE na imagem atual (se baseando no indice)
-    // Isso faz ela aparecer (por causa do CSS e JS)
+    // Ativa o slide atual
     slides[i].classList.add("active");
-    // Ativa também a bolinha 
-    dots[i].classList.add("active");
+
+    // Ativa o dot atual
+    bolinha[i].classList.add("active");
 }
